386a637e9df571148122294f87ab56f62a0ea4c5
Bootcamp 1 <cjansson@gmail.com>
2019-04-09 21:24:57 -0400

    app.py

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from sqlalchemy import create_engine
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
+
 app = Flask(__name__)
 
 
@@ -37,47 +39,32 @@ def index():
     return render_template("index.html")
 
 
-# @app.route("/names")
-# def names():
-#     """Return a list of sample names."""
-
-#     # Use Pandas to perform the sql query
-#     stmt = db.session.query(FoodRecall).state
-#     df = pd.read_sql_query(stmt, db.session.bind)
-
-#     # Return a list of the column names (sample names)
-#     return jsonify(list(df.columns)[2:])
+@app.route("/Category/")
+def category():
+    """Return the homepage."""
+    return render_template("Category/category.html")
 
 
-# @app.route("/metadata/<sample>")
-# def sample_metadata(sample):
-#     """Return the MetaData for a given sample."""
-#     sel = [
-#         Samples_Metadata.sample,
-#         Samples_Metadata.ETHNICITY,
-#         Samples_Metadata.GENDER,
-#         Samples_Metadata.AGE,
-#         Samples_Metadata.LOCATION,
-#         Samples_Metadata.BBTYPE,
-#         Samples_Metadata.WFREQ,
-#     ]
+@app.route("/classification/")
+def classification():
+    """Return the homepage."""
+    return render_template("classification.html")
 
-#     results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
 
-    # # Create a dictionary entry for each row of metadata information
-    # sample_metadata = {}
-    # for result in results:
-    #     sample_metadata["sample"] = result[0]
-    #     sample_metadata["ETHNICITY"] = result[1]
-    #     sample_metadata["GENDER"] = result[2]
-    #     sample_metadata["AGE"] = result[3]
-    #     sample_metadata["LOCATION"] = result[4]
-    #     sample_metadata["BBTYPE"] = result[5]
-    #     sample_metadata["WFREQ"] = result[6]
-
-    # print(sample_metadata)
-    # return jsonify(sample_metadata)
-
+@app.route("/firm/<year>")
+def firm(year):
+    stmt = db.session.query(FoodRecall).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+    yr = int(year)
+    sample_data = df.loc[df["year_reported"] == yr, ["recalling_firm","state","status","year_reported"]]   
+    sample_data = sample_data.groupby(['recalling_firm']).size().reset_index(name='counts')
+    sample_data = sample_data.sort_values(['counts'], ascending=False)
+    sample_data = sample_data.head(15)
+    data = {
+        "recalling_firm": sample_data.recalling_firm.tolist(),
+        "counts": sample_data.counts.tolist(),
+    }
+    return jsonify(data)
 
 @app.route("/samples/<year>")
 def samples(year):
@@ -96,8 +83,6 @@ def samples(year):
         "recalling_firm": sample_data.recalling_firm.tolist(),
         "sample_values": sample_data.country.tolist(),
         "state": sample_data.state.tolist(),
-        #"report_date": sample_data.report_date.tolist(),
-        #"termination_date": sample_data.termination_date.tolist(),
         "status": sample_data.status.tolist(),
         "voluntary_mandated": sample_data.voluntary_mandated.tolist(),
         "classification": sample_data.classification.tolist(),
@@ -105,6 +90,36 @@ def samples(year):
     }
     #print(yr)
     #print(jsonify(data))
+    return jsonify(data)
+
+@app.route("/status/<year>")
+def status(year):
+    stmt = db.session.query(FoodRecall).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+    yr = int(year)
+    sample_data = df.loc[df["year_reported"] == yr, ["status","year_reported"]]   
+    sample_data = sample_data.groupby(['status']).size().reset_index(name='counts')
+    sample_data = sample_data.sort_values(['counts'], ascending=False)
+    sample_data = sample_data.head(15)
+    data = {
+        "status": sample_data.status.tolist(),
+        "counts": sample_data.counts.tolist(),
+    }
+    return jsonify(data)
+
+@app.route("/states/<year>")
+def states(year):
+    stmt = db.session.query(FoodRecall).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+    yr = int(year)
+    sample_data = df.loc[df["year_reported"] == yr, ["recalling_firm","state","status","year_reported"]]
+    sample_data = sample_data.groupby(['state']).size().reset_index(name='counts')
+    sample_data = sample_data.sort_values(['counts'], ascending=False)
+    sample_data = sample_data.head(15)
+    data = {
+        "state": sample_data.state.tolist(),
+        "counts": sample_data.counts.tolist(),
+    }
     return jsonify(data)
 
 
